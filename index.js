ebf64c834367e137ce8ebe8534947d151e4ae380
den <d.n.voropaev@gmail.com>
2020-08-17 18:53:35 +0300

    added draft solution

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
-export default async function intArrayToCompactString (array) {
-    return
+export default function intArrayToCompactString (array) {
+
+    return new Promise (resolve => {
+
+        let prev, consecutive = 0, result = ''
+
+        for (let int of array) {
+
+            if (prev === undefined)
+                result += int
+
+            else if (int - prev <= 1)
+                consecutive += 1
+
+            else {
+                result += (consecutive > 1 ? '-' : ',') + (consecutive > 0 ? prev + ',' : '') + int
+                consecutive = 0
+            }
+
+            prev = int
+        }
+
+        if (consecutive > 0)
+            result += (consecutive > 1 ? '-' : ',') + prev
+
+        resolve (result)
+    })
 }
--- test/index.js
+++ test/index.js
@@ -6,6 +6,8 @@ describe ('Int Array to Compact String', () => {
     it ('creates a compact string from an array of integer numbers', async () => {
 
         const tests = [
+            [[], ''],
+            [[1,1,1,1,1,1,1], '1-1'], // the task does not specify such cases
             [[1,2,3,4,5,6,7,8], '1-8'],
             [[1,3,4,5,6,7,8], '1,3-8'],
             [[1,3,4,5,6,7,8,10,11,12], '1,3-8,10-12'],
@@ -19,8 +21,8 @@ describe ('Int Array to Compact String', () => {
             [[1,3,5,7,9,11], '1,3,5,7,9,11'],
         ]
 
-        await Promise.all (tests.map (([test, expected]) => (
-            expect (intArrayToCompactString (test)).to.equal (expected)
+        await Promise.all (tests.map (([test, expected]) => intArrayToCompactString (test).then (actual =>
+            expect (actual).to.equal (expected)
         )))
 
         return
