8e81b842ee85f54dfb98986fce9e20002dabdfbc
den <d.n.voropaev@gmail.com>
2020-08-17 16:59:21 +0300

    added default test cases

---
+++ test/index.js
@@ -0,0 +1,28 @@
+import { expect } from 'chai'
+import intArrayToCompactString from '../index.js'
+
+describe ('Int Array to Compact String', () => {
+
+    it ('creates a compact string from an array of integer numbers', async () => {
+
+        const tests = [
+            [[1,2,3,4,5,6,7,8], '1-8'],
+            [[1,3,4,5,6,7,8], '1,3-8'],
+            [[1,3,4,5,6,7,8,10,11,12], '1,3-8,10-12'],
+            [[1,2,3], '1-3'],
+            [[1,2], '1,2'],
+            [[1,2,4], '1,2,4'],
+            [[1,2,4,5,6], '1,2,4-6'],
+            [[1,2,3,7,8,9,15,17,19,20,21], '1-3,7-9,15,17,19-21'],
+            [[1,2,3,4,5,6,100,1091,1999,2000,2001,2002], '1-6,100,1091,1999-2002'],
+            [[1], '1'],
+            [[1,3,5,7,9,11], '1,3,5,7,9,11'],
+        ]
+
+        await Promise.all (tests.map (([test, expected]) => (
+            expect (intArrayToCompactString (test)).to.equal (expected)
+        )))
+
+        return
+    })
+})
